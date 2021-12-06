929826aeeafde8bb395074c2c1cbaf835f405580
Frederique Hsu (徐赞) <frederique.hsu@outlook.com>
2021-12-07 02:40:14 +0800

    使用C++语言和Python语言实现Factory_Method模式

---
+++ Factory_Method/build.sh
@@ -0,0 +1,7 @@
+# Script name   : build.sh
+# Description   : this shell script is used to build current project.
+#
+
+g++ Factory_Method.cpp Factory_Method.hpp Main.cpp -o Factory_Method
+
+./Factory_Method
