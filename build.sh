e59de2614bfe9202baddd3b0fedb80c71f9ad237
Frederique Hsu (徐赞) <xuz2@microport.com>
2022-02-13 21:29:29 +0800

    Add 3 .cpp source files.

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh 
+#
+#   File name   : build.sh
+#   Description : This shell script tells how to build the program.
+#
+
+g++ Recurring_Template_Pattern.cpp -std=c++17 -o Recurring_Template_Pattern
+
+g++ Mixin_Inheritance.cpp -std=c++17 -o Mixin_Inheritance
+
+g++ Properties.cpp -std=c++17 -o Properties
