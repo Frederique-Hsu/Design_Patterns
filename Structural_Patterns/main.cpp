e836d076a94a61faf95efaff91aaa44f24c0f351
Frederique Hsu <frederique.hsu@outlook.com>
2024-08-27 03:53:32 +0800

    Add the CMakeLists.txt scripts for Structural_Patterns root project and all its sub-projects

---
+++ Structural_Patterns/main.cpp
@@ -0,0 +1,26 @@
+/*!
+ *  \file       main.cpp
+ *  \brief      Make the in-depth research on the Structural design patterns, this file is common file,
+ *              which implements the main() portal for all sub-projects.
+ *  \author     Frederique Hsu  (frederique.hsu@outlook.com)
+ *  \date       Tue.    27 Aug. 2024
+ *  \copyright  2024    All rights reserved.
+ *  
+ */
+
+
+#include <catch2/catch_session.hpp>
+#include <catch2/catch_test_macros.hpp>
+#include <iostream>
+
+int main(int argc, char* argv[], char* envp[])
+{
+    std::cout << "Current C++ standard no. is " << __cplusplus << std::endl;
+    
+    return Catch::Session().run(argc, argv);
+}
+
+TEST_CASE("Unit testing for Structural Patterns", "[Structural_Patterns]")
+{
+    CHECK(true);
+}
