9a624e62c67e02bb28c348c32f5743f49ed0e973
Frederique Hsu <frederique.hsu@outlook.com>
2024-09-13 13:07:03 +0800

    Remove the old folder for Factory_Method

--- Creational_Patterns/Factory_Method/Product.c++
+++
@@ -1,5 +0,0 @@
-#include "Product.h++"
-
-Product::~Product()
-{
-}
--- Creational_Patterns/Factory_Method/build.sh
+++
@@ -1,7 +0,0 @@
-# Script name   : build.sh
-# Description   : this shell script is used to build current project.
-#
-
-g++ Factory_Method.cpp Factory_Method.hpp Main.cpp -o Factory_Method
-
-./Factory_Method
