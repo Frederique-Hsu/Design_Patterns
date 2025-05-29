d6cc6e6960d3ceadf9b1809b42ba6d0245f60a18
Frederique Hsu <frederique.hsu@outlook.com>
2025-05-30 03:39:55 +0800

    Modify the Behavioral_Patterns project.

--- Behavioral_Patterns/main.cpp
+++ Behavioral_Patterns/main.cpp
@@ -11,10 +11,14 @@
 
 #include <gtest/gtest.h>
 
-int main(int argc, char* argv[], char* envp[])
-{
-    std::cout << "Current C++ standard no. is " << __cplusplus << std::endl;
-    
+int main(int argc, char* argv[])
+{    
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
+}
+
+TEST(UTest4CppStandard, CheckCppStandardNo)
+{
+    std::cout << "Current C++ standard no. is " << __cplusplus << std::endl;
+    std::cout << "Current program is " << sizeof(void*) * 8 << "-bits." << std::endl;
 }
