93e790842354df652df16dda754715d0015f3e46
Frederique Hsu (徐赞) <zan.xu@hozoneauto.com.cn>
2025-07-09 18:03:57 +0800

    调整根目录的CMakeLists.txt和两个依赖项boost, TinyXml2的cmake脚本，使得Common子项目Common.lib库在Windows Visual Studio 2022环境下构建成功。

--- cmake/boost.cmake
+++ cmake/boost.cmake
@@ -1,26 +1,54 @@
 cmake_minimum_required(VERSION 3.20)
 
+set(BOOST_SRC_DIR			${CMAKE_SOURCE_DIR}/3rd_party_libraries/boost-src)
+set(BOOST_STATE_DIR			${CMAKE_BINARY_DIR}/boost-state)
+set(BOOST_INSTALL_DIR		${CMAKE_BINARY_DIR}/boost-install)
 
-set(BOOST_SRC_DIR           ${CMAKE_SOURCE_DIR}/3rd_party_libraries/boost-src)
-set(BOOST_BUILD_DIR         ${CMAKE_BINARY_DIR}/boost-build)
-set(BOOST_INSTALL_DIR       ${CMAKE_BINARY_DIR}/boost-install)
-set(BOOST_DOWNLOAD_DIR      ${CMAKE_BINARY_DIR}/boost-download)
+
+if (NOT EXISTS "${BOOST_SRC_DIR}/.git")
+	message(STATUS "Initial the boost repo downloading...")
+
+	file(MAKE_DIRECTORY ${BOOST_SRC_DIR})
+
+	if (${CMAKE_SYSTEM_NAME} STREQUAL "Windows" AND MSVC)
+		set(GIT_EXECUTABLE "C:/Program Files/Git/cmd/git.exe")
+		# find_program(GIT_EXECUTABLE NAMES git git.exe REQUIRED)
+	elseif (${CMAKE_SYSTEM_NAMME} STREQUAL "Linux")
+		find_program(GIT_EXECUTABLE git REQUIRED)
+	endif()
+
+	message(STATUS "GIT_EXECUTABLE = ${GIT_EXECUTABLE}")
+
+	set(GIT_TAG		boost-1.86.0)
+
+	execute_process(
+		COMMAND ${GIT_EXECUTABLE} clone --recurse-submodules --depth 10 --branch ${GIT_TAG} https://github.com/boostorg/boost.git ${BOOST_SRC_DIR}
+		WORKING_DIRECTORY		${CMAKE_CURRENT_BINARY_DIR}
+		RESULT_VARIABLE			GIT_CLONE_RESULT
+	)
+
+	if (NOT GIT_CLONE_RESULT EQUAL 0)
+		message(FATAL_ERROR "Failed to git clone boost repository.")
+	endif()
+endif()
+
 
 include(ExternalProject)
 
 ExternalProject_Add(Boost
-    GIT_REPOSITORY      https://github.com/boostorg/boost.git
-    GIT_TAG             boost-1.86.0
-    GIT_SHALLOW         TRUE
-    GIT_PROGRESS        TRUE
-    SOURCE_DIR          ${BOOST_SRC_DIR}
-    BINARY_DIR          ${BOOST_BUILD_DIR}
-    DOWNLOAD_DIR        ${BOOST_DOWNLOAD_DIR}
-    UPDATE_DISCONNECTED TRUE
-    CMAKE_ARGS
-        -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
-        -DBUILD_SHARED_LIBS=ON
-        -DCMAKE_INSTALL_PREFIX=${BOOST_INSTALL_DIR}
-    BUILD_ALWAYS        TRUE
-    STEP_TARGETS        install
+	PREFIX					${BOOST_STATE_DIR}
+	SOURCE_DIR				${BOOST_SRC_DIR}
+	DOWNLOAD_COMMAND		""
+	GIT_CONFIG				"core.worktree=${BOOST_SRC_DIR}"
+	UPDATE_DISCONNECTED		ON
+	CMAKE_ARGS
+		-DCMAKE_BUILD_TYPE=Debug
+		-DBUILD_SHARED_LIBS=OFF
+		-DCMAKE_INSTALL_PREFIX=${BOOST_INSTALL_DIR}
+	BUILD_ALWAYS			ON
+	LOG_BUILD				ON
+	STEP_TARGETS			install
 )
+
+set(BOOST_INCLUDE_DIR		${BOOST_INSTALL_DIR}/include)
+set(BOOST_LIB_DIR			${BOOST_INSTALL_DIR}/lib)
--- cmake/tinyxml2.cmake
+++ cmake/tinyxml2.cmake
@@ -1,41 +1,54 @@
 cmake_minimum_required(VERSION 3.20)
 
 
-set(TINYXML2_SRC_DIR            ${CMAKE_SOURCE_DIR}/3rd_party_libraries/tinyxml2-src)
-set(TINYXML2_BUILD_DIR          ${CMAKE_BINARY_DIR}/tinyxml2-build)
-set(TINYXML2_INSTALL_DIR        ${CMAKE_BINARY_DIR}/tinyxml2-install)
-set(TINYXML2_DOWNLOAD_DIR       ${CMAKE_BINARY_DIR}/tinyxml2-download)
+set(TINYXML2_SRC_DIR		${CMAKE_SOURCE_DIR}/3rd_party_libraries/tinyxml2-src)
+set(TINYXML2_STATE_DIR		${CMAKE_BINARY_DIR}/tinyxml2-state)
+set(TINYXML2_INSTALL_DIR	${CMAKE_BINARY_DIR}/tinyxml2-install)
 
-if (FALSE)
-    include (FetchContent)
-    set(FETCHCONTENT_UPDATES_DISCONNECTED   TRUE)
 
-    FetchContent_Declare(TinyXml2
-        GIT_REPOSITORY      https://github.com/leethomason/tinyxml2.git
-        GIT_TAG             10.0.0
-        GIT_SHALLOW         TRUE
-        GIT_PROGRESS        TRUE
-        SOURCE_DIR          ${TINYXML2_SRC_DIR}
-        # BINARY_DIR        ${TINYXML2_BUILD_DIR}
-    )
-    FetchContent_MakeAvailable(TinyXml2)
-else()
-    include(ExternalProject)
+if (NOT EXISTS "${TINYXML2_SRC_DIR}/.git")
+	message(STATUS "Initialize the tinyxml2 repository downloading...")
 
-    ExternalProject_Add(TinyXml2
-        GIT_REPOSITORY      https://github.com/leethomason/tinyxml2.git
-        GIT_TAG             10.0.0
-        GIT_SHALLOW         TRUE
-        GIT_PROGRESS        TRUE
-        SOURCE_DIR          ${TINYXML2_SRC_DIR}
-        BINARY_DIR          ${TINYXML2_BUILD_DIR}
-        DOWNLOAD_DIR        ${TINYXML2_DOWNLOAD_DIR}
-        UPDATE_DISCONNECTED TRUE
-        CMAKE_ARGS
-            -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
-            -DCMAKE_INSTALL_PREFIX=${TINYXML2_INSTALL_DIR}
-            -DBUILD_SHARED_LIBS=ON
-        BUILD_ALWAYS        TRUE
-        STEP_TARGETS        install
-    )
+	file(MAKE_DIRECTORY ${TINYXML2_SRC_DIR})
+
+	if (${CMAKE_SYSTEM_NAME} STREQUAL "Windows" AND MSVC)
+		set(GIT_EXECUTABLE "C:/Program Files/Git/cmd/git.exe")
+		# find_program(GIT_EXECUTABLE NAMES git git.exe REQUIRED)
+	elseif (${CMAKE_SYSTEM_NAMME} STREQUAL "Linux")
+		find_program(GIT_EXECUTABLE git REQUIRED)
+	endif()
+
+	message(STATUS "GIT_EXECUTABLE = ${GIT_EXECUTABLE}")
+
+	set(GIT_TAG		10.0.0)
+
+	execute_process(
+		COMMAND ${GIT_EXECUTABLE} clone --depth 10 --branch ${GIT_TAG} https://github.com/leethomason/tinyxml2.git ${TINYXML2_SRC_DIR}
+		WORKING_DIRECTORY		${CMAKE_CURRENT_BINARY_DIR}
+		RESULT_VARIABLE			GIT_CLONE_RESULT
+	)
+
+	if (NOT GIT_CLONE_RESULT EQUAL 0)
+		message(FATAL_ERROR "Failed to git clone the tinyxml2 repository.")
+	endif()
 endif()
+
+
+include(ExternalProject)
+
+ExternalProject_Add(TinyXml2
+	PREFIX					${TINYXML2_STATE_DIR}
+	SOURCE_DIR				${TINYXML2_SRC_DIR}
+	DOWNLOAD_COMMAND		""
+	GIT_CONFIG				"core.worktree=${TINYXML2_SRC_DIR}"
+	UPDATE_DISCONNECTED		ON
+	CMAKE_ARGS
+		-DCMAKE_BUILD_TYPE=Release
+		-DBUILD_SHARED_LIBS=ON
+		-DCMAKE_INSTALL_PREFIX=${TINYXML2_INSTALL_DIR}
+	BUILD_ALWAYS			ON
+	STEP_TARGETS			install
+)
+
+set(TINYXML2_INCLUDE_DIR		${TINYXML2_INSTALL_DIR}/include)
+set(TINYXML2_LIB_DIR			${TINYXML2_INSTALL_DIR}/lib)
