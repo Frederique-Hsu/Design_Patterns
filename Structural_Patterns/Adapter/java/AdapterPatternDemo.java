9e12794e12832b6af461bc30445802b8e4530289
Frederique Hsu (徐贊) <frederique.hsu@outlook.com>
2025-08-11 17:52:38 +0800

    Implement the Adapter Pattern in Java

---
+++ Structural_Patterns/Adapter/java/AdapterPatternDemo.java
@@ -0,0 +1,21 @@
+/**
+ *  @file       AdapterPatternDemo.java
+ *  @brief
+ *
+ */
+
+
+import AudioPlayer.*;
+
+public class AdapterPatternDemo
+{
+    public static void main(String[] args)
+    {
+        AudioPlayer audioPlayer = new AudioPlayer();
+
+        audioPlayer.play("mp3", "Beyond the horizon.mp3");
+        audioPlayer.play("mp4", "Alone.mp4");
+        audioPlayer.play("vlc", "Far far away.vlc");
+        audioPlayer.play("avi", "Mind me.avi");
+    }
+}
