d6ccf17dbee33336b7638812aa4535ad18bba0dd
Frederique Hsu (徐贊) <frederique.hsu@outlook.com>
2025-09-01 17:08:13 +0800

    研究Composite组合模式，创建Composite和Test_Composite两个子项目。

--- Structural_Patterns/main.cpp
+++ Structural_Patterns/main.cpp
@@ -1,6 +1,6 @@
 /*!
  *  \file       main.cpp
- *  \brief      Make the in-depth research on the Structural design patterns, this file is common file,
+ *  \brief      Make the in-depth research on the Structural design patterns, this file is the common file,
  *              which implements the main() portal for all sub-projects.
  *  \author     Frederique Hsu  (frederique.hsu@outlook.com)
  *  \date       Tue.    27 Aug. 2024
@@ -15,6 +15,8 @@ int main(int argc, char* argv[])
 {    
     (void)argc;
     (void)argv;
+    std::cout << "Current C++ standard no. is " << __cplusplus << std::endl;
+    std::cout << "Current program is " << sizeof(void*) * 8 << "-bits.\n" << std::endl;
 
     return EXIT_SUCCESS;
 }
