c841292cd7b2e5281ffe249f52b4eaed911c47e3
Frederique Hsu (徐赞) <frederique_hsu@Frederiques-MacBook-Air.local>
2026-01-11 03:25:44 +0800

    Write unit test case to verify how the Decorator Pattern works.

--- testing/Test_Structural_Patterns/Test_Decorator/test_decorator.cpp
+++ testing/Test_Structural_Patterns/Test_Decorator/test_decorator.cpp
@@ -7,17 +7,24 @@
 
 #include <catch2/catch_test_macros.hpp>
 
+#include <iostream>
+#include <memory>
+
 #include "Structural_Patterns/Decorator/cpp/ConcreteComponent.hpp"
 #include "Structural_Patterns/Decorator/cpp/ConcreteDecorator.hpp"
 #include "Structural_Patterns/Decorator/cpp/Client.hpp"
+#include "Structural_Patterns/Decorator/cpp/Window.hpp"
+#include "Structural_Patterns/Decorator/cpp/TextView.hpp"
+#include "Structural_Patterns/Decorator/cpp/BorderDecorator.hpp"
+#include "Structural_Patterns/Decorator/cpp/ScrollDecorator.hpp"
 
-#include <iostream>
-#include <memory>
 
 TEST_CASE("UTester for Decorator Pattern", "[Decorator]")
 {
     SECTION("Check how Decorator Pattern works")
     {
+        using namespace dive_into_design_patterns;
+
         Component *simple = new ConcreteComponent;
         std::cout << "Client: I have got a simple component: " << std::endl;
 
@@ -28,6 +35,8 @@ TEST_CASE("UTester for Decorator Pattern", "[Decorator]")
 
     SECTION("Check how decorators wrap not only simple components, but also other decorators as welll")
     {
+        using namespace dive_into_design_patterns;
+
         std::shared_ptr<Component> simple(new ConcreteComponent);
         std::shared_ptr<Component> decorator1(new ConcreteDecoratorA(simple.get()));
         std::shared_ptr<Component> decorator2(new ConcreteDecoratorB(decorator1.get()));
@@ -35,5 +44,15 @@ TEST_CASE("UTester for Decorator Pattern", "[Decorator]")
         std::cout << "Client: Now I have got a decorated component: " << std::endl;
         Client::execute(decorator2.get());
         std::cout << "\n" << std::endl;
+    }
+
+    SECTION("Validate how decorator pattern works, when user want to build a text view")
+    {
+        using namespace gof_design_patterns;
+
+        std::unique_ptr<Window> window(new Window);
+        std::unique_ptr<TextView> text_view = std::make_unique<TextView>();
+
+        window->setContents(new BorderDecorator(new ScrollDecorator(text_view.get()), 1));
     }
 }
